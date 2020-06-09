2f6a11f81f336d6eb01c6a4dc214929c2154fd55
dhu758 <dhu758@naver.com>
2020-06-09 16:57:08 +0900

    front page

--- bookmark/models.py
+++ bookmark/models.py
@@ -8,5 +8,5 @@ class Bookmark(models.Model):
     def __str__(self):
         return "이름 : " + self.site_name + ", 주소 : " + self.url
 
-    def get_absolute_url(self):
-        return reverse('detail', arg=[self.id])
+    # def get_absolute_url(self):
+        # return reverse('detail', arg=[self.id])
