4b7cafb118acc69f5cac9bc2c658a420a8697d1a
dhu758 <dhu758@naver.com>
2020-06-10 15:42:48 +0900

    북마크 생정, 수정, 삭제 기능

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
@@ -8,5 +8,5 @@ class Bookmark(models.Model):
     def __str__(self):
         return "이름 : " + self.site_name + ", 주소 : " + self.url
 
-    # def get_absolute_url(self):
-        # return reverse('detail', arg=[self.id])
+    def get_absolute_url(self):
+        return reverse('detail', args=[self.id])
